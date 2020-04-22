6c4c58a9bbc97fe1a23d61bd1e796b54e92eb151
Luke <olooook@outlook.com>
2020-04-22 16:55:33 +0800

    添加log

--- tools/trainer.py
+++ tools/trainer.py
@@ -1,6 +1,7 @@
 import json
 import os
 from datetime import datetime
+from functools import partial
 
 import torch
 from torch.nn import MSELoss
@@ -11,6 +12,7 @@ from tools.config import Config
 from tools.metrics import Metrics
 from tools.data import make_loaders
 from tools.model import make_matgcn
+from tools.utils import log_to_file, make_saved_dir, save_statistics, save_model, save_history
 
 
 class Trainer:
@@ -18,11 +20,15 @@ class Trainer:
 		self.conf = conf
 		self.history = []
 		self.requires_move = conf.device_for_model != conf.device_for_data
+		self.saved_dir = make_saved_dir(conf.saved_dir)
+		self.log_for_train = partial(log_to_file, f'{self.saved_dir}/train.log')
+		self.log_for_validate = partial(log_to_file, f'{self.saved_dir}/validate.log')
 		# load
 		print('Loading data...')
-		data_loaders, self.statistics = make_loaders(conf)
+		data_loaders, statistics = make_loaders(conf)
 		self.data_for_train = data_loaders['train']
 		self.data_for_validate = data_loaders['validate']
+		# save_statistics(statistics, f'{self.saved_dir}/statistics.pth')
 		# creat
 		print('Creating model...')
 		self.matgcn = make_matgcn(conf)
@@ -32,19 +38,24 @@ class Trainer:
 	def run(self):
 		# train
 		print('Training...')
+		best = float('inf')
 		self.history.clear()
 		for epoch in range(self.conf.epochs):
 			print(f"EPOCH: {epoch + 1}")
 			self.history.append({
-				'train': self.train_epoch(),
-				'validate': self.validate_epoch()
+				'train': self.train_epoch(epoch),
+				'validate': self.validate_epoch(epoch)
 			})
-		self.save()
+			MAE = self.history[-1]['validate']['metrics']['MAE']
+			if epoch > int(0.2 * self.conf.epochs) and MAE < best:
+				save_model(self.matgcn, f'{self.saved_dir}/model-{MAE:.2f}.pkl')
+				best = MAE
+		save_history(self.history, f'{self.saved_dir}/history.json')
 
-	def train_epoch(self):
+	def train_epoch(self, epoch):
 		total_loss, count = .0, 0
 		with tqdm(total=len(self.data_for_train), desc='TRAIN', unit='batches') as bar:
-			for x, y in self.data_for_train:
+			for b, (x, y) in enumerate(self.data_for_train):
 				if self.requires_move:
 					x, y = x.to(self.conf.device_for_model), y.to(self.conf.device_for_model)
 				self.optimizer.zero_grad()
@@ -58,15 +69,17 @@ class Trainer:
 				# update progress bar
 				bar.update()
 				bar.set_postfix(loss=f'{total_loss / count:.2f}')
+				# log to file
+				self.log_for_train(epoch=epoch, batch=b, loss=loss.item())
 		return {'loss': total_loss / count}
 
 	@torch.no_grad()
-	def validate_epoch(self):
+	def validate_epoch(self, epoch):
 		metrics = Metrics()
 		total_loss, count = .0, 0
 		self.matgcn.eval()
 		with tqdm(total=len(self.data_for_validate), desc='VALIDATE', unit='batches') as bar:
-			for x, y in self.data_for_validate:
+			for b, (x, y) in enumerate(self.data_for_validate):
 				if self.requires_move:
 					x, y = x.to(self.conf.device_for_model), y.to(self.conf.device_for_model)
 				pred = self.matgcn(x)
@@ -80,22 +93,6 @@ class Trainer:
 				bar.set_postfix(**{
 					k: f'{v:.2f}' for k, v in metrics.status.items()
 				}, loss=f'{total_loss / count:.2f}')
+				self.log_for_validate(epoch=epoch, batch=b, loss=loss.item(), **metrics.status)
 		self.matgcn.train()
 		return {'loss': total_loss / count, 'metrics': metrics.status}
-
-	def save(self, history=True, statistics=False, model=False):
-		if not os.path.exists(self.conf.saved_dir):
-			os.mkdir(self.conf.saved_dir)
-		saved_dir = os.path.join(self.conf.saved_dir, datetime.now().strftime('%m-%d-%H-%M-%S'))
-		if not os.path.exists(saved_dir):
-			os.mkdir(saved_dir)
-		if history:
-			# save history
-			with open(f'{saved_dir}/history.json', 'w') as f:
-				f.write(json.dumps(self.history))
-		if statistics:
-			# save normalizers
-			torch.save(self.statistics, f'{saved_dir}/statistics.pth')
-		if model:
-			# save model
-			torch.save(self.matgcn, f'{saved_dir}/model.pkl')
--- tools/utils.py
+++ tools/utils.py
@@ -1,3 +1,7 @@
+import json
+import os
+from datetime import datetime
+
 import torch
 
 
@@ -10,3 +14,31 @@ def norm_adj_matrix(adj_file, n_vertices, device='cpu'):
 
 	D_rsqrt = torch.sum(A, dim=1).rsqrt().diag()
 	return D_rsqrt @ A @ D_rsqrt  # D^{-1/2} A D^{-1/2}
+
+
+def make_saved_dir(saved_dir):
+	if not os.path.exists(saved_dir):
+		os.mkdir(saved_dir)
+	saved_dir = os.path.join(saved_dir, datetime.now().strftime('%m-%d-%H-%M-%S'))
+	if not os.path.exists(saved_dir):
+		os.mkdir(saved_dir)
+	return saved_dir
+
+
+def log_to_file(file, **kwargs):
+	with open(file, 'a') as f:
+		f.write(','.join([f'{k}={v}' for k, v in kwargs.items()]))
+		f.write('\n')
+
+
+def save_model(file, model):
+	torch.save(model, file)
+
+
+def save_history(file, history):
+	with open(file, 'w') as f:
+		f.write(json.dumps(history))
+
+
+def save_statistics(file, statistics):
+	torch.save(statistics, file)
