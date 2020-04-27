f6e36cbabde2034c853129af17df8c7d17b13ccb
Luke <olooook@outlook.com>
2020-04-27 08:52:19 +0800

    精简化

--- tools/metrics.py
+++ tools/metrics.py
@@ -3,21 +3,19 @@ import torch
 
 class Metrics:
 	def __init__(self):
-		self.total = {}.fromkeys(['AE', 'SE'], .0)
-		self.count = 0
+		self.AE, self.SE, self.count = .0, .0, 0
 
 	def update(self, pred, y):
-		self.total['AE'] += torch.abs(pred - y).sum().item()
-		self.total['SE'] += torch.pow(pred - y, 2).sum().item()
+		self.AE += torch.abs(pred - y).sum().item()
+		self.SE += torch.pow(pred - y, 2).sum().item()
 		self.count += pred.nelement()
 
 	def clear(self):
-		self.total = {}.fromkeys(['AE', 'SE'], .0)
-		self.count = 0
+		self.AE, self.SE, self.count = .0, .0, 0
 
 	@property
 	def status(self):
 		return {
-			'MAE': self.total['AE'] / self.count,
-			'RMSE': (self.total['SE'] / self.count) ** .5
+			'MAE': self.AE / self.count,
+			'RMSE': (self.SE / self.count) ** .5
 		}
