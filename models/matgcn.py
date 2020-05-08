2ca205bec61154040f11ac536b3d446551eff01a
Luke <olooook@outlook.com>
2020-05-08 19:35:28 +0800

    .

--- models/matgcn.py
+++ models/matgcn.py
@@ -20,9 +20,9 @@ class SAttention(Module):
 		:return: [B, N, N]
 		"""
 		# k_{n,t} = q_{n,t} = x_{i,n,t} \alpha^{i}
-		K = Q = torch.einsum('bint,i->bnt', x, self.alpha)  # [B, N, T]
+		k = q = torch.einsum('bint,i->bnt', x, self.alpha)  # [B, N, T]
 		# [B, N, T] @ [T, T] @ [B, T, N]
-		return torch.softmax(K @ self.W @ Q.transpose(1, 2), dim=-1)  # [B, N, N]
+		return torch.softmax(k @ self.W @ q.transpose(1, 2), dim=-1)  # [B, N, N]
 
 
 class GCNBlock(Module):
@@ -37,9 +37,9 @@ class GCNBlock(Module):
 		:param x: [B, C_i, N, T]
 		:return: [B, C_o, N, T]
 		"""
-		A = self.s_att(x) * self.A  # [B, N, N]
+		A_att = self.A * self.s_att(x)  # [B, N, N]
 		# [B, N, N] @ [T, B, N, C_i] @ [C_i, C_o]
-		x_out = A @ x.permute(3, 0, 2, 1) @ self.W.T  # [T, B, N, C_o]
+		x_out = A_att @ x.permute(3, 0, 2, 1) @ self.W.T  # [T, B, N, C_o]
 		return x_out.permute(1, 3, 2, 0)  # [B, C_o, N, T]
 
 
@@ -56,8 +56,8 @@ class TAttention(Module):
 		:return: [B, C, N, T]
 		"""
 		# k_{t,n} = q_{t,n} = x_{i,n,t} \alpha^{i}
-		K = Q = torch.einsum('bint,i->btn', x, self.alpha)  # [B, T, N]
-		A = torch.softmax((K @ self.W1.T) @ (Q @ self.W2.T).transpose(1, 2), dim=-1)  # [B, T, T]
+		k = q = torch.einsum('bint,i->btn', x, self.alpha)  # [B, T, N]
+		A = torch.softmax((k @ self.W1.T) @ (q @ self.W2.T).transpose(1, 2), dim=-1)  # [B, T, T]
 		# y_{c,n,t} = a_{t,i} x_{c,n,i}
 		return torch.einsum('bti,bcni->bcnt', A, x)  # [B, C, N, T]
 
@@ -104,8 +104,8 @@ class CAttention(Module):
 		:return: [B, C, N, T]
 		"""
 		# k_{c,t} = q_{c,t} = x_{c,i,t} \alpha^{i}
-		K = Q = torch.einsum('bcit,i->bct', x, self.alpha)  # [B, C, T]
-		A = torch.softmax(K @ self.W @ Q.transpose(1, 2), dim=-1)  # [B, C, C]
+		k = q = torch.einsum('bcit,i->bct', x, self.alpha)  # [B, C, T]
+		A = torch.softmax(k @ self.W @ q.transpose(1, 2), dim=-1)  # [B, C, C]
 		# y_{c,n,t} = a_{c,i} x_{i,n,t}
 		return torch.einsum('bci,bint->bcnt', A, x)  # [B, C, N, T]
 
@@ -132,8 +132,9 @@ class MATGCNBlock(Module):
 class MATGCNLayer(Module):
 	def __init__(self, blocks, **kwargs):
 		super(MATGCNLayer, self).__init__()
-		self.blocks = Sequential(*[MATGCNBlock(**block, **kwargs) for block in blocks])
-		self.ln = LayerNorm([kwargs['in_timesteps']])
+		self.seq = Sequential(*[
+			MATGCNBlock(**block, **kwargs) for block in blocks
+		], LayerNorm([kwargs['in_timesteps']]))
 		self.fc = Conv2d(kwargs['in_timesteps'], kwargs['out_timesteps'], [1, blocks[-1]['out_channels']])
 
 	def forward(self, x: FloatTensor):
@@ -141,8 +142,7 @@ class MATGCNLayer(Module):
 		:param x: [B, C_i, N, T_i]
 		:return: [B, C_o, N, T_o]
 		"""
-		x = self.blocks(x)  # [B, C_o, N, T_o]
-		x = self.ln(x)  # [B, C_o, N, T_o]
+		x = self.seq(x)  # [B, C_o, N, T_o]
 		x = self.fc(x.transpose(1, 3))  # [B, T_o, N, 1]
 		return x[..., 0].transpose(1, 2)  # [B, N, T_o]
 
